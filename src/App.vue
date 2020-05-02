453a25f7973dc086a388e1771432295d2d76ece2
garrettmichael <garesnap@gmail.com>
2020-05-02 18:17:14 -0400

    Init Commit

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view />
   </div>
 </template>
 
 <style>
+body {
+  background-color: #F7F8F7;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -16,17 +15,12 @@
   text-align: center;
   color: #2c3e50;
 }
-
-#nav {
-  padding: 30px;
+button {
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  background-color: #525153;
+  color: #F7F8F7;
+  border-color: #525153;
+  border-radius: .5rem;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <span v-if="!editting">{{ text }}</span>
+    <input v-if="editting" type="text" :value="text" />
+<div class="flexme">
+    <button @click="edit()">{{ Edit }}</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      editting: false,
+      Edit: "Edit",
+    };
+  },
+  props: {
+    text: String,
+  },
+  methods: {
+    edit() {
+      if (this.editting) {
+        this.editting = false;
+        this.Edit = "Edit";
+      } else {
+        this.editting = true;
+        this.Edit = "Done";
+      }
+    },
+  },
+  computed: {},
+};
+</script>
+
+<style scoped>
+span {
+  margin-right: 2em;
+}
+.flexme {
+  display: inline-flex;
+  justify-content: flex-end;
+}
+</style>
